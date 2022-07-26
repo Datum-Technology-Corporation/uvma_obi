84df38493a656a3ae5c0e18018c09ae2808cf982
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-26 16:40:47 -0400

    updated regression for new CLI

--- dv/uvmt_obi_st/bin/sanity_regression.sh
+++ dv/uvmt_obi_st/bin/sanity_regression.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 ########################################################################################################################
-## Copyright 2021 Datum Technology Corporation
+## Copyright 2021-2022 Datum Technology Corporation
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ########################################################################################################################
 
 
 # Launched from uvml project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_obi_st
-mio sim     uvmt_obi_st -t reads      -s 1 -c
-mio sim     uvmt_obi_st -t writes     -s 1 -c
-mio sim     uvmt_obi_st -t all_access -s 1 -c
+mio sim     uvmt_obi_st -CE
+mio sim     uvmt_obi_st -S -t reads      -s 1 -c
+mio sim     uvmt_obi_st -S -t writes     -s 1 -c
+mio sim     uvmt_obi_st -S -t all_access -s 1 -c
 mio results uvmt_obi_st results
 mio cov     uvmt_obi_st
